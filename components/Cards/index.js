ecdda3ef89c4d81c506cb7f6b39ad126b55a787a
Cassandra Villarreal <cass2u94@gmail.com>
2020-06-14 21:12:24 -0400

    cards done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,31 +20,125 @@
 // Use your function to create a card for each of the articles and add the card to the DOM.
 
 axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
-    .then(function(response) {
-        console.log(response.data.articles)
-    })
+    
+.then(function(response) {
 
-function CreateCard(headline, author, name) {
-    let card = document.createElement("div");
-    card.classList.add("card")
+    console.log(response.data.articles.javascript)
+
+    let cards = document.querySelector(".cards-container") 
+    
+    response.data.articles.javascript.forEach( item => {
+
+    let jscard = document.createElement("div");
+        jscard.classList.add("card")
+        cards.appendChild(jscard)
     
 
     let Headline = document.createElement("div");
     Headline.classList.add("headline")
-    Headline.textContent = headline
+    Headline.textContent = item.headline
+    jscard.appendChild(Headline)
 
 
     let Author = document.createElement("div")
     Author.classList.add("author")
+    Headline.appendChild(Author)
     
     let pic = document.createElement("div");
     pic.classList.add("img-container")
     Author.appendChild(pic)
     let image = document.createElement("img");
-    image.src = author
+    image.src = item.authorPhoto
     pic.appendChild(image)
 
     let Name = document.createElement("span");
-    Name.textContent = "By " + name
+    Name.textContent = "By " + item.authorName
     pic.appendChild(Name);
-}
+})
+
+response.data.articles.bootstrap.forEach( item => {
+
+    let bootcard = document.createElement("div");
+        bootcard.classList.add("card")
+        cards.appendChild(bootcard)
+    
+
+    let Headline = document.createElement("div");
+    Headline.classList.add("headline")
+    Headline.textContent = item.headline
+    bootcard.appendChild(Headline)
+
+
+    let Author = document.createElement("div")
+    Author.classList.add("author")
+    Headline.appendChild(Author)
+    
+    let pic = document.createElement("div");
+    pic.classList.add("img-container")
+    Author.appendChild(pic)
+    let image = document.createElement("img");
+    image.src = item.authorPhoto
+    pic.appendChild(image)
+
+    let Name = document.createElement("span");
+    Name.textContent = "By " + item.authorName
+    pic.appendChild(Name);
+}) 
+response.data.articles.jquery.forEach( item => {
+
+    let jqcard = document.createElement("div");
+        jqcard.classList.add("card")
+        cards.appendChild(jqcard)
+    
+
+    let Headline = document.createElement("div");
+    Headline.classList.add("headline")
+    Headline.textContent = item.headline
+    jqcard.appendChild(Headline)
+
+
+    let Author = document.createElement("div")
+    Author.classList.add("author")
+    Headline.appendChild(Author)
+    
+    let pic = document.createElement("div");
+    pic.classList.add("img-container")
+    Author.appendChild(pic)
+    let image = document.createElement("img");
+    image.src = item.authorPhoto
+    pic.appendChild(image)
+
+    let Name = document.createElement("span");
+    Name.textContent = "By " + item.authorName
+    pic.appendChild(Name);
+})
+response.data.articles.node.forEach( item => {
+
+    let nodecard = document.createElement("div");
+        nodecard.classList.add("card")
+        cards.appendChild(nodecard)
+    
+
+    let Headline = document.createElement("div");
+    Headline.classList.add("headline")
+    Headline.textContent = item.headline
+    nodecard.appendChild(Headline)
+
+
+    let Author = document.createElement("div")
+    Author.classList.add("author")
+    Headline.appendChild(Author)
+    
+    let pic = document.createElement("div");
+    pic.classList.add("img-container")
+    Author.appendChild(pic)
+    let image = document.createElement("img");
+    image.src = item.authorPhoto
+    pic.appendChild(image)
+
+    let Name = document.createElement("span");
+    Name.textContent = "By " + item.authorName
+    pic.appendChild(Name);
+})
+
+})
